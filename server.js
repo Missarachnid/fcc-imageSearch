4fa79488478ae51fbcdf7d0cbe900b450e32ed09
Glitch (corgi) <none>
2018-04-05 17:18:17 +0000

    🏜😾 Checkpoint ./server.js:503759/2312 ./public/index.html:503759/40

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-//const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const GoogleSearch = require('google-search');
 const history = require('./models/history');
 const app = express();
 const axios = require('axios');
 app.use(bodyParser.json());
-//app.use(cors());
+app.use(cors());
 
 //connect to MLab
 const uri = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+':'+process.env.DB_PORT+'/'+process.env.DB;
@@ -18,6 +18,12 @@ mongoose.connect(uri).then((err, res) => {
     console.log(err);
   }
 });
+var db = mongoose.connection;
+db.createCollection("historys", {
+    capped: true,
+    size: 5242880,
+    max: 5000
+  });
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -29,36 +35,18 @@ app.get('/api/imagesearch/:searchTerm*', (req, res) => {
   let data = new history({
     term: searchTerm,
   });
-  data.save
-  /*const url = `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_ID}&q=${searchTerm}&searchType=image&alt=json`;
-  axios.get(url)
-  .then(response => {
-  console.log('Response', response);
-  })
-  .catch(error => {
-    console.log('Error', error);
-  })*/
+  data.save(err => {
+    if(err){
+      console.log('Error', err);
+    } else {
+      console.log('success', data);
+    }
+  });
+  
   
 });
 
-/*var googleSearch = new GoogleSearch({
-  key: process.env.GOOGLE_API_KEY,
-  cx: process.env.GOOGLE_ID
-});
- 
- 
-googleSearch.build({
-  q: "corgis",
-  num: 100,
-  searchType: "image"
-}, (error, response) =>  {
-  if(error){
-    console.log("Error", error);
-  } else{
-    console.log("Response", response.items.length);
-  }
-  
-});*/
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
@@ -80,17 +68,3 @@ var listener = app.listen(process.env.PORT, function () {
 &searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}
 &imgDominantColor={imgDominantColor?}&alt=json"*/
 
-/*main:
-url, snippet, thumbnail, context
-
-recent
-term, when
-{"term":"lolcats funny","when":"2018-04-05T13:54:11.479Z"}
-
-{"url":"https://llwproductions.files.wordpress.com/2012/05/funny-cat-pictures-with-caption-lolcats-one-dumbazh-away.jpg","snippet":"This is Motley News' 1,000th Post! Time to Celebrate with Funnies ...","thumbnail":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvgMU-uS_Czv-Gw7eHFYf2Ue6wMoNX00AImn8Ljk0ZHKzjG7_us5FCG6K5","context":"https://motleynews.net/2012/05/01/this-is-motley-news-1000th-post-time-to-celebrate-with-funnies/"}
-
-kind for each object, its in the items array
-url = image.context link
-snippet = snippet
-thumbnail = image.thumbnail link*/
-/*https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={'+process.env.GOOGLE_ID+'}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"*/
