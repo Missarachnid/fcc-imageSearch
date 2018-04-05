53c3448dfb0ed7e40b3572d40662e1228eb16ef3
Glitch (corgi) <none>
2018-04-05 16:02:28 +0000

    🎾😼 Checkpoint ./server.js:503759/823

--- server.js
+++ server.js
@@ -17,11 +17,12 @@ const uri = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.H
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('api/imagesearch/:searchTerm*', (request, response) => {
-  
+app.get('api/imagesearch/:searchTerm*', (req, res) => {
+  const {searchTerm} = req.params;
+  console.log(searchTerm);
 });
 
-var googleSearch = new GoogleSearch({
+/*var googleSearch = new GoogleSearch({
   key: process.env.GOOGLE_API_KEY,
   cx: process.env.GOOGLE_ID
 });
@@ -38,7 +39,7 @@ googleSearch.build({
     console.log("Response", response.items.length);
   }
   
-});
+});*/
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
@@ -73,3 +74,4 @@ kind for each object, its in the items array
 url = image.context link
 snippet = snippet
 thumbnail = image.thumbnail link*/
+/*https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={'+process.env.GOOGLE_ID+'}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"*/
