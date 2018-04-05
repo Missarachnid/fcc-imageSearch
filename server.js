1ef1807eedcb20715518efbd5f529d9bde54cf3d
Glitch (hollow-basket) <none>
2018-04-05 00:41:10 +0000

    🗼👗 Checkpoint ./server.js:503759/291

--- server.js
+++ server.js
@@ -20,3 +20,9 @@ app.get("/", function (request, response) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//I can get the image URLs, alt text and page urls for a set of images relating to a given search string.
+//I can paginate through the responses by adding a ?offset=2 parameter to the URL.
+//I can get a list of the most recently submitted search strings.
+// /api/imagesearch/
+// /api/latest
