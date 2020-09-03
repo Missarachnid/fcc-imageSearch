63103002b6dcdbf1fa05ce007d69236ed969a168
Glitch (corgi) <none>
2020-09-03 01:18:18 +0000

    🎇🌴 Checkpoint ./server.js:503759/25

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors());
 
 //connect to MLab
 const uri = 'mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST+'/'+process.env.DB;
+console.log('uri', uri);
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then((err, res) => {
   if(err){
     console.log(err);
